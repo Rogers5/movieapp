0b0b431431f44d53828d373b6350fdd3ae7bb42f
AlienCao <caoyujia2000@gmail.com>
2017-01-12 16:55:30 +0800

    detail over

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -7,7 +7,9 @@ Page({
     detail: {},
     movie:{},
     film:{},
-    hidden: false
+    hidden: false,
+    counts:10,
+    display:"quantity-decrease"
   },
   onLoad: function (options) {
     this.fetchData(options.id);
@@ -33,5 +35,36 @@ Page({
         }, 300);
       }
     });
+  },
+  decreaseNum: function (e) {
+    if(this.data.counts !== 1){
+      this.setData({
+          counts: this.data.counts - 1,
+          display:"quantity-decrease"
+      });
+    }else{
+      this.setData({
+          display:"decrease-none"
+      });
+    }
+    
+    //console.log("ID:"+id);
+
+  },
+  increaseNum: function (e) {
+    //var id = "show"+e.currentTarget.id;
+    if(this.data.counts == 1){
+      this.setData({
+          counts: this.data.counts + 1,
+          display:"quantity-decrease"
+        });
+    }else{
+        this.setData({
+          counts: this.data.counts + 1
+        });
+    }
+    
+    //console.log("ID:"+id);
+
   }
 })
